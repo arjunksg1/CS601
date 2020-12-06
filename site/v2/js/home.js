46f26a0fc5770165d76a8c58f3059b980c9ccec2
Arjun Sengupta <arjunksg1@gmail.com>
2020-12-06 00:28:00 -0500

    test

---
+++ site/v2/js/home.js
@@ -0,0 +1,22 @@
+window.onload = init;
+
+function init(){
+    displayHelloMessage();
+}
+
+function displayHelloMessage(){
+    let messageElements = document.getElementsByClassName("default-hide");
+    let helloElement = document.getElementById("message-display");
+    let index = 0;
+    setInterval(
+        function(){
+            if(index === messageElements.length){
+                index = 0;
+            }
+            helloElement.innerHTML = messageElements[index].innerHTML;
+            index++;
+        },
+        4000
+    );
+}
+
